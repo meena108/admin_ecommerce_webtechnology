37f8babf4cf4e44f1c5e6ca2939259256ad6af8e
Meena Chand <meena.hgmd@gmail.com>
2024-04-01 08:05:55 -0700

    contact api and tested with ok result

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
+use App\Http\Controllers\Admin\ContactControlller;
 
+
+//get visitor
 Route::get('/getVisitor', [VisitorController::class, 'GetVisitorDetails']);
+//contact page router
+
+Route::post('/postcontact', [ContactControlller::class, 'PostContactDetails']);
