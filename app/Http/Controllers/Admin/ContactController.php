05e46a7dacd1a7d12e0c19aa373a2487d34d2559
Meena Chand <meena.hgmd@gmail.com>
2024-05-14 04:05:38 -0700

    Rename ContactControlller.php to ContactController.php

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Contact;
 
-class ContactControlller extends Controller
+class ContactController extends Controller
 {
     public function PostContactDetails(Request $request){
 
